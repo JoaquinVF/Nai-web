e211df23a92b549855e995512fa06a52c206274e
Joaquin Villanueva <joaquinvillanuevafarber@gmail.com>
2021-05-20 18:05:31 -0300

    theme add

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+
 function showContent() {
     element = document.getElementById("menu");
     check = document.getElementById("checkbox");
     if (check.checked) {
         element.style.display='block';
-        element.style.transition= "opacity 0.5s";
     }
     else {
         element.style.display='none';
+    }
+}
+
+function changeTheme(){
+    theme = document.getElementById("theme");
+    if (theme.checked){
+        background = document.getElementById("top");
+        background.style.background = '#212121';
+        background.style.color = 'white';
+    } else {
+        background.style.background = 'white';
+        background.style.color = 'black';
     }
 }
