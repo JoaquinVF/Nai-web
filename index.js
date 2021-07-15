6236e7cfe062b8a6219580656fe0aa9ba89efa15
Joaquin Villanueva <74026054+JoaquinVF@users.noreply.github.com>
2021-07-14 22:48:22 -0300

    theme added

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
+const element = document.getElementById("menu");
+const check = document.getElementById("checkbox");
+const theme = document.getElementById('theme');
+const body = document.getElementById('body');
+const icon = document.querySelector('.icon');
+
 function showContent() {
-    element = document.getElementById("menu");
-    check = document.getElementById("checkbox");
     if (check.checked) {
         element.style.display='block';
-        element.style.transition= "opacity 0.5s";
     }
     else {
         element.style.display='none';
     }
+}
+
+check.addEventListener('click', (e)=>{
+    showContent();
+});
+
+theme.addEventListener('change', function(){
+    if (this.checked) {
+        document.body.classList.toggle("dark");
+        localStorage.setItem('dark', this.checked);
+        icon.style.backgroundImage =  "url('./imgs/icons8-bombilla-globo-48.png')";
+    } else {
+        document.body.classList.remove('dark');  
+        localStorage.removeItem('dark');
+        icon.style.backgroundImage =  "url('./imgs/icons8-luna-creciente-48.png')";
+   }
+});
+
+if(localStorage.getItem('dark')) {
+    document.body.classList.add('dark');
+    icon.style.backgroundImage =  "url('./imgs/icons8-bombilla-globo-48.png')";
 }
