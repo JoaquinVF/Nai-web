4914a258f1136f28a43c1b84a1489e4ccd11021c
Joaquin Villanueva <74026054+JoaquinVF@users.noreply.github.com>
2021-08-06 16:09:30 -0300

    cambios en el theme, icons, colors, carpetas nuevas parte 1

--- index.js
+++ index.js
@@ -2,14 +2,36 @@ const element = document.getElementById("menu");
 const check = document.getElementById("checkbox");
 const theme = document.getElementById('theme');
 const body = document.getElementById('body');
-const icon = document.querySelector('.icon');
+const bombilla = document.querySelector('.bombilla');
+const luna = document.querySelector('.luna');
+const main = document.querySelector('main');
+const load = document.querySelector('.load');
+const instagram = document.querySelector('.social');
+
+
+function loading(){
+    setTimeout(function(){
+        load.style.opacity = '0';
+        load.style.display = 'none';
+
+        main.style.display = 'block';
+        setTimeout(()=> (main.style.opacity = '1'), 50)
+    }, 1500)
+}
+loading();
 
 function showContent() {
     if (check.checked) {
         element.style.display='block';
+        setTimeout((e)=>{
+            element.style.opacity='1';
+        },50)
     }
     else {
-        element.style.display='none';
+        setTimeout((e)=>{
+            element.style.opacity='0';
+            element.style.display='none';
+        },50)
     }
 }
 
@@ -21,15 +43,26 @@ theme.addEventListener('change', function(){
     if (this.checked) {
         document.body.classList.toggle("dark");
         localStorage.setItem('dark', this.checked);
-        icon.style.backgroundImage =  "url('./imgs/icons8-bombilla-globo-48.png')";
+        luna.style.display = 'none';
+        bombilla.style.display = 'block';
+        instagram.style.backgroundImage= "url('./imgs/icons8_instagram_old_100px.png')";        
     } else {
         document.body.classList.remove('dark');  
         localStorage.removeItem('dark');
-        icon.style.backgroundImage =  "url('./imgs/icons8-luna-creciente-48.png')";
+        luna.style.display = 'block';
+        bombilla.style.display = 'none';
+        instagram.style.backgroundImage= "url('./imgs/icons8-instagram-viejo-100.png')";        
    }
 });
 
-if(localStorage.getItem('dark')) {
-    document.body.classList.add('dark');
-    icon.style.backgroundImage =  "url('./imgs/icons8-bombilla-globo-48.png')";
+function darkOrLight(){
+    if(localStorage.getItem('dark')) {
+        document.body.classList.add('dark');
+    } else {
+    luna.style.display = 'block';
+    bombilla.style.display = 'none';
+    instagram.style.backgroundImage= "url('./imgs/icons8-instagram-viejo-100.png')"; 
+    }
 }
+
+darkOrLight();
